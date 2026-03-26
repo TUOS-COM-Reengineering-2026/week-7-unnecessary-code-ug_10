51d26c4c3f88056f182454bff01cc34950c0263f
Anastasiia Lets <alets1@sheffield.ac.uk>
2026-03-26 23:02:17 +0000

    feat: finding method in a file and dot plot for 2 files - exact copies

--- code_clones.py
+++ code_clones.py
@@ -1,6 +1,12 @@
 import os
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+import numpy as np
 
-def read_file(file_path: str) -> str:
+ASSIGNMENT = '='
+BOOLEAN = ['==', '!=','<=','>=','<','>']
+
+def read_file(file_path: str) -> list[str]:
     """
     Read the contents of a file.
 
@@ -8,11 +14,17 @@ def read_file(file_path: str) -> str:
     :return: The contents of the file.
     """
 
+    print(f"Read file {file_path}")
+    read = False
+
     if not os.path.isfile(file_path):
         raise ValueError(f"{file_path} is not a file.")
 
     with open(file_path, 'r') as file:
-        return file.read()
+        # if not read:
+        #     print(file.readline())
+        #     read = True
+        return file.readlines()
 
 def compute_jaccard_similarity(a: str, b: str) -> float:
     """
@@ -23,60 +35,119 @@ def compute_jaccard_similarity(a: str, b: str) -> float:
     :return: The Jaccard similarity between the two programs.
     """
 
-    a_content = read_file(a).splitlines()
-    b_content = read_file(b).splitlines()
+    a_content = read_file(a)
+    b_content = read_file(b)
 
-    # TODO: Implement the rest of this function
+    a_set = set(a_content)
+    b_set = set(b_content)
 
-    return 0
+    return len(a_set & b_set)/len(a_set | b_set)
 
 def visualise_dot_plot(a: str, b: str) -> str:
-    a_content = read_file(a).splitlines()
-    b_content = read_file(b).splitlines()
+    a_content = read_file(a)
+    b_content = read_file(b)
 
     plot = '-' * 80 + '\n'
     for i in range(len(a_content)):
-        plot += f'x{i}: {a_content[i]}\n'
+        plot += f'x{i}: {a_content[i]}'
     plot += '-' * 80 + '\n'
     for j in range(len(b_content)):
-        plot += f'y{j}: {b_content[j]}\n'
+        plot += f'y{j}: {b_content[j]}'
     plot += '-' * 80 + '\n'
 
-    # TODO: Implement the rest to return the dot plot for the two given programs (a: x-axis, b: y-axis)
-
-    # Below is a sample output:
-    '''
-    --------------------------------------------------------------------------------
-    x0: def calculate_total(values: list):
-    x1:     total = 0
-    x2:     for value in values:
-    x3:         if value < 0:
-    x4:             continue
-    x5:         total += value
-    x6:     return total
-    --------------------------------------------------------------------------------
-    y0: def calculate_total(values: list):
-    y1:     total = 0
-    y2:     for value in values:
-    y3:         if value < 0:
-    y4:             print("Negative value!")
-    y5:             continue
-    y6:         total += value
-    y7:     return total
-    --------------------------------------------------------------------------------
-        x0	x1	x2	x3	x4	x5	x6	
-    y0	*	 	 	 	 	 	 	
-    y1	 	*	 	 	 	 	 	
-    y2	 	 	*	 	 	 	 	
-    y3	 	 	 	*	 	 	 	
-    y4	 	 	 	 	 	 	 	
-    y5	 	 	 	 	*	 	 	
-    y6	 	 	 	 	 	*	 	
-    y7	 	 	 	 	 	 	*	
-    --------------------------------------------------------------------------------
-    '''
-
     plot += '\t'
+    for i in range(len(a_content)):
+        plot += f"x{i+1}\t"
+    plot += '\n'
+    for j in range(len(b_content)):
+        plot += f"y{j+1}\t"
+        for i in range(len(a_content)):
+            if a_content[i] == b_content[j]:
+                plot += f"*\t"
+            else:
+                plot += f" \t"
+        plot += '\n'
+
     plot += '-' * 80
 
     return plot.strip()
+
+
+def draw_graph(a: str, b: str):
+    column_content = read_file(a)
+    row_content = read_file(b)
+
+    xdata = np.array([])
+    ydata = np.array([])
+
+    for j in range (len(row_content)):
+        for i in range(len(column_content)):
+            if column_content[i] == row_content[j]:
+                xdata = np.append(xdata, [i+1])
+                ydata = np.append(ydata, [j+1])
+
+
+    fig, ax = plt.subplots()
+    ax.plot(xdata, ydata, 'o')
+    ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+    ax.invert_yaxis()
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    
+    plt.show()
+
+
+def find_method(path_file, method_name) -> list[str]:
+    IDENTIFIER = "def"
+    content = read_file(path_file)
+
+    method_code = []
+    reading_code = False
+
+    for line in content:
+        if reading_code:
+            method_code.append(line)
+
+        if line.startswith(IDENTIFIER):
+            name = line.split(' ')[1]
+            if name.strip() == method_name and not reading_code:
+                method_code.append(line)
+                reading_code = True
+            else:
+                reading_code = False
+                
+                
+    print(len(method_code))
+
+
+
+if __name__ == "__main__":
+    path_dir = r"d:\modules25_26\Reengineering\arrow-repo\arrow"
+    test_path = "d:\modules25_26\Reengineering\week-7-unnecessary-code-ug_10"
+    name = "range"
+    file = "dead_code.py"
+    file2 = "dying_code.py"
+    
+
+    line1 = "t = 5"
+    line2 = "x = 5"
+
+    #is_same_variable_name(line1,line2)
+
+    path_file = os.sep.join([test_path, file2])
+    find_method(path_file, "example_function():")
+    # print(path_file1)
+    path_file2 = os.sep.join([test_path, file2])
+
+    #draw_graph(path_file, path_file2)
+
+    #graph = visualise_dot_plot(path_file, path_file2)
+    #print(graph)
+
+    # print("\n")
+
+    # similarity = compute_jaccard_similarity(path_file1, path_file2)
+
+    # print(similarity)
+
+
